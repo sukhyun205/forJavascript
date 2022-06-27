5ec1fa2dfe4d2a1455a77de3a4d58ee2d8eb9283
Sukhyun Kwon <sukhyun205@gmail.com>
2022-06-27 11:58:43 +0900

    fixed reset button & modified UI functions

--- public/numberGuess.js
+++ public/numberGuess.js
@@ -16,6 +16,9 @@ let computerNum = 0;
 let resultArea = document.getElementById("result-area")
 //User input
 let userInput = document.getElementById("user-input") //id는 반드시 copy해서 넣기
+userInput.addEventListener("focus",function(){
+    userInput.value="";
+})
 //button go
 let playButton = document.getElementById("playButton")
 playButton.addEventListener("click", play) //함수 play도 매개변수로 넣을수 있다는 점 중요@@, 매개변수로 함수를 넣을땐 ()를 넣지않는다. 왜? ()매개변수를 넣으면 함수가 바로 실행되어버리기 때문에. //addEventListener(클릭, 함수) 
@@ -29,14 +32,14 @@ let chanceArea = document.getElementById("chance-area")
 //
 let chance=5;
 //
-let gameOver=false;
+let history = []
+
 
 function pickRandomNum(){
     computerNum = Math.floor((Math.random()*100)+1);
     console.log("정답은",computerNum,"(미리보기)")
     console.log("게임 시작!!")
 }
-pickRandomNum()
 
 function play(){
     let userValue = userInput.value; //유저가 입력한 값을 가져오고싶을땐, 변수.value로 호출
@@ -44,53 +47,82 @@ function play(){
     
     let chanceValue = chanceArea.value;
 
+    let gameOver=false; //gameOver가 여기있어야 reset이 정상적으로 되네
+
+    if(userValue<1 || userValue>100){
+        resultArea.textContent="エラ！！1~100の数字のみ";
+        console.log(resultArea.textContent)
+        return; //return으로 함수를 멈춰줘야, chance가 안깍인다.
+    }
+
+    //선생님 방법: .inclues(target)으로 좀더 수월하게 중복처리 가능
+    if(history.includes(userValue)){
+        resultArea.textContent="중복!!";
+        return;
+    }
+
+
+    //중복값 처리 내 방법
+    // for(let i=0; i<history.length; i++){
+    //     if(userValue==history[i]){
+    //         resultArea.textContent="入力値が重複します！！";
+    //         console.log(resultArea.textContent)
+    //         return
+    //     }
+    // }
+    
+
     chance--; //아 클릭할때마다 1씩 줄게해야하는구나
 
     chanceValue = chance;
 
+    chanceArea.textContent = `남은기회: ${chance}번`
+
     if(userValue<computerNum && userValue>0){
         resultArea.textContent="UP!!" //html에서 띄울 메세지를 변수 resultArea에 저장
         console.log(resultArea.textContent)
-        
-        chanceArea.textContent=chanceValue;
-        console.log("기회가",chanceArea.textContent+"번 남았습니다.")
     }
     else if(userValue>computerNum && userValue<=100){
         resultArea.textContent="DOWN!!"
         console.log(resultArea.textContent)
     }
-    else if(userValue==computerNum){
+    else{
         commentArea.textContent="おめでとうございます！！"
         resultArea.textContent="正解!!"
         console.log(resultArea.textContent)
+        playButton.disabled = true;
+        chanceArea.textContent="";
     }
-    else{
-        resultArea.textContent="エラ！！1~100の数字のみ"
-        console.log(resultArea.textContent)
-    }
+
+    history.push(userValue)
+    console.log(history)
 
     if(chance<1){
         gameOver = true;
     }
 
     if(gameOver==true){
+        resultArea.textContent="Game Over, Please press the reset button"
         playButton.disabled = true;
-        resultArea.textContent="Game Over, Please press reset button"
         chanceArea.textContent="";
+
     }
 
 }
 
 function reset(){
-    chance=5;
+    playButton.disabled = false;
     userInput.value="";
-    resultArea.textContent=""; //유저 입력값을 초기화하고,
+    chanceArea.textContent="残りの機会:5"; //유저 입력값을 초기화하고,
+    resultArea.textContent=""
     commentArea.textContent="1~100の数字を入力してください。"
     console.log(resultArea.textContent)
     pickRandomNum(); //새로운 랜덤숫자를 생성하는게 reset
-    
-
+    chance=5;
+    history=[];
 }
+
+pickRandomNum()
 
 //getelementsby 연습
 // let arr_menu = document.getElementsByClassName("menu")
