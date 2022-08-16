3ea3a8a2f9a8dcb375d8ea4cc7e3386d71872d1f
Sukhyun Kwon <sukhyun205@gmail.com>
2022-08-16 22:42:41 +0900

    add getNewsByTopic()

--- public/API.js
+++ public/API.js
@@ -1,30 +1,46 @@
 let news = []
+let menus = document.querySelectorAll(".menus button")
+
+console.log("munues: ",menus)
+
+menus.forEach((item)=>{
+    item.addEventListener("click", (event)=>getNewsByTopic(event))
+})
 
 const getLatestNews = async()=>{
-    let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=10') //new URL()함수는 URL의 정보를 분석해준다.
-    // console.log(url)
 
+    let url = new URL('https://api.newscatcherapi.com/v2/latest_headlines?countries=JP&page_size=10') //new URL()함수는 URL의 정보를 분석해준다.
     let header = new Headers({"x-api-key":"klvI5IGf3ZG2W6NuI0muNA6bLS33bNOy6xR3fip1NlM"})
-
     let response = await fetch(url,{headers:header}) //async와 await은 한 세트 
     //url, header를 통해 데이터를 fetch해오기도 전에 data를 출력시키면 data 변수를 불러올 수 없게된다.
     let data = await response.json()
     // console.log("데이터:",data)
 
     news = data.articles
-    console.log(news)
+    console.log("토픽뉴스:",news)
 
     render();
 }
 
+const getNewsByTopic = async(event) =>{ //await이랑 async는 짝짜꿍. 무조건 같이 온다.
+
+    // console.log("클릭됨", event.target.textContent)
+
+    let topic = event.target.textContent.toLowerCase()
+    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=JP&page_size=10&topic=${topic}`)
+    let header = new Headers({"x-api-key":"klvI5IGf3ZG2W6NuI0muNA6bLS33bNOy6xR3fip1NlM"})
+    let response = await fetch(url,{headers:header}) //async와 await은 한 세트 
+    let data = await response.json()
+
+    news = data.articles
+    console.log("토픽뉴스:",news)
+
+    render();
+
+}
 
 
-//기본적인 API를 호출하는 방법.
-// const callAPI = async() => {
-//     let url = new URL('url주소')
-//     let header
-//     let response = await fetch("url", {headers:header})
-// }
+
 
 // 사이드 메뉴바 함수
 const openNav = () => {
@@ -70,3 +86,10 @@ const render = ()=>{
 
 
 getLatestNews();
+
+//기본적인 API를 호출하는 방법.
+// const callAPI = async() => {
+//     let url = new URL('url주소')
+//     let header
+//     let response = await fetch("url", {headers:header})
+// }
